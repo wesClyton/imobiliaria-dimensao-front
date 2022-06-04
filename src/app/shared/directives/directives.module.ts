80b4adef6b2c3b3c7b4382875f33487ef5c8f40e
Felipe Mantovani <felipexmantovani@gmail.com>
2022-06-04 11:55:05 -0300

    Código do anúncio UpperCase

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -4,20 +4,23 @@ import { CepDirective } from './cep/cep.directive';
 import { CurrencyBrDirective } from './currency-br/currency-br.directive';
 import { M2Directive } from './m2/m2.directive';
 import { PhoneDirective } from './phone/phone.directive';
+import { UpperCaseDirective } from './uppercase/uppercase.directive';
 
 @NgModule({
   declarations: [
     PhoneDirective,
     CepDirective,
     M2Directive,
-    CurrencyBrDirective
+    CurrencyBrDirective,
+    UpperCaseDirective
   ],
   imports: [NgxMaskModule.forRoot()],
   exports: [
     PhoneDirective,
     CepDirective,
     M2Directive,
-    CurrencyBrDirective
+    CurrencyBrDirective,
+    UpperCaseDirective
   ]
 })
 export class DirectivesModule {}
---
+++ src/app/shared/directives/uppercase/uppercase.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+@Directive({
+  selector: '[appUpperCase]'
+})
+export class UpperCaseDirective implements OnInit, OnDestroy {
+
+  private subscription = new Subscription();
+
+  constructor(
+    private readonly ngControl: NgControl,
+    private readonly elementRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+    this.transformValue((this.elementRef.nativeElement as HTMLInputElement).value);
+    this.subscription.add(this.ngControl.control?.valueChanges.subscribe(value => this.transformValue(value)));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private transformValue(value: string): void {
+    this.ngControl.control?.setValue(value.toUpperCase(), { emitEvent: false });
+  }
+
+}
